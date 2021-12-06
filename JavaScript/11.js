32c0dcb71c78d6e4f813e4689907b0567ab2a78a
Dovilė <dovile.keparute@gmail.com>
2021-12-06 17:41:54 +0200

    DPM 11

--- JavaScript/11.js
+++ JavaScript/11.js
@@ -1,5 +1,6 @@
 const input = document.querySelector('input');
 console.log(input);
+const spalva = input.style.color = 'red';
 
 const submit = document.getElementsByTagName('button')[3];
 console.log(submit);
@@ -8,11 +9,11 @@ const span = document.querySelector('span');
 console.log(span);
 
 submit.addEventListener('click', function() {
-const text = input.value;
-span.innerText = text;
+span.innerText =input.value;
 input.value = input.innerText;
-// arba
-// input.value = '';
 })
+
+
+
 
 
