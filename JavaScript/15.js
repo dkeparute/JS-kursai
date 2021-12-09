c14a65518c5f53253d90a7c4b1c88ee6589df979
Dovilė <dovile.keparute@gmail.com>
2021-12-09 18:02:47 +0200

    DOM 15

---
+++ JavaScript/15.js
@@ -0,0 +1,31 @@
+function generuotiSocials(selector, iconList) {
+    // validation
+    if (typeof selector !== 'string' || selector.length <1) {
+        console.error('ivestas selectorius negali buti tuscias stringas ir privalo buti tik stringas');
+        return false;
+    }
+    
+    // logic
+    const selectorDOM = document.querySelector(selector);
+    let htmlElements = '';
+    for (let i = 0; i < iconList.length; i++) {
+        htmlElements += `
+        <i class="fa fa-${iconList[i]}"></i>`;
+    }
+
+    // logic validation
+if(!selectorDOM){
+    console.error('neteisingas selectorius');
+    return false;
+}
+
+    // result return
+    selectorDOM.innerHTML = htmlElements;
+}
+
+const icons = ['facebook', 'instagram', 'twitter', 'linkedin'];
+
+generuotiSocials('.socials', icons);
+// PATESTAVIMAS
+// generuotiSocials('', icons);
+// generuotiSocials('labas', icons);
